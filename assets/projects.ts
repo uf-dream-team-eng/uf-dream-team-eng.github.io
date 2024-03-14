cbbafe21a276a25449081e64a29874c9d1860b2d
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2024-03-14 02:10:08 -0400

    Auto-generated commit

---
+++ assets/projects.ts
@@ -0,0 +1,7 @@
+export const projects = [
+    { name: "EKG Team",
+      desc: "The EKG Team is collaborating with Dr. Julian to develop a low-cost portable EKG machine for educational demonstrations. Explore our EKG monitor below:",
+      img: "/assets/EKG.png",
+      link: "/ekg"
+    },
+  ]
